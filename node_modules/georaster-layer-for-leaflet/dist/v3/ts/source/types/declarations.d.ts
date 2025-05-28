518f9c138b9b7ec30e4095d8d353a3ec4205cfc5
cowrrr <costly-chant.00@icloud.com>
2025-05-28 17:35:45 +0300

    git

--- node_modules/georaster-layer-for-leaflet/dist/v3/ts/source/types/declarations.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "utm-utils/src/isUTM.js";
-declare module "utm-utils/src/getProjString.js";
-declare module "proj4-fully-loaded";
-declare module "geocanvas";
-declare module "geo-extent";
-declare module "snap-bbox";
--- node_modules/geotiff/src/decoder.worker.js
+++
@@ -1,9 +0,0 @@
-import { expose } from 'threads/worker';
-import { getDecoder } from './compression';
-
-function decode(fileDirectory, buffer) {
-  const decoder = getDecoder(fileDirectory);
-  return decoder.decode(fileDirectory, buffer);
-}
-
-expose(decode);
